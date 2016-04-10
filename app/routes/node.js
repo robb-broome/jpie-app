e4478f474931961ba5d6c4a843dd7d6dcb597d95
robb-broome <robb@teamvoice.com>
2016-04-10 07:46:42 -0500

    models/reouts/components/

--- app/routes/node.js
+++ app/routes/node.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
-  model(params) {
-    return this.store.findRecord('node', params.id);
+  model() {
+    return { 
+      uuid: 'llkadlfkjasdlkj',
+      text: 'this is the text'
+      };
+    // return this.store.findRecord('node', params.id);
   }
 });
 
