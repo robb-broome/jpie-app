d35a7f8aad401395d4922625de00b8177a308e79
robb-broome <robb@teamvoice.com>
2016-04-11 07:44:06 -0500

    broken route/model

--- app/routes/nodes.js
+++ app/routes/nodes.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return Ember.$.getJSON('http://jinglepie.com/api/nodes');
+  model() {
+   return this.store.find('node');
   }
 });
